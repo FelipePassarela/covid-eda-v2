f3762e2ad03d1218e00dc1d8269c3822c7be61d6
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-07-31 09:56:07 -0300

    refactor(explain): ♻️ enhance SHAP explanation process with separate function and improved data handling

--- src/covid/cli/explain.py
+++ src/covid/cli/explain.py
@@ -2,9 +2,12 @@ from pathlib import Path
 from typing import Any
 
 import joblib
+import pandas as pd
 import shap
 import typer
 from imblearn.pipeline import Pipeline as ImblearnPipeline
+from shap import Explanation
+from sklearn.base import BaseEstimator
 from sklearn.model_selection import TunedThresholdClassifierCV
 from sklearn.pipeline import Pipeline as SklearnPipeline
 
@@ -17,19 +20,26 @@ def main() -> None:
 
 
 def explain(
-    pipeline_path: Path, data_path: Path = constants.INTERIM_TRAIN_DATA_PATH
+    pipeline_path: Path,
+    train_path: Path = constants.INTERIM_TRAIN_DATA_PATH,
+    test_path: Path = constants.INTERIM_TEST_DATA_PATH,
 ) -> None:
-    data = load_data(data_path)
-    X, y = split_features_and_target(data)
+    train_data = load_data(train_path)
+    X_train, _ = split_features_and_target(train_data)
 
     threshold_model = joblib.load(pipeline_path)
     pipeline = unwrap_threshold_model(threshold_model)
     preprocessor, classifier = split_fitted_pipeline(pipeline)
 
-    X_transformed = preprocessor.transform(X)
+    test_data = load_data(test_path)
+    X_test, _ = split_features_and_target(test_data)
 
-    explainer = shap.TreeExplainer(classifier)
-    explanation = explainer(X_transformed)
+    X_test_transformed = preprocessor.transform(X_test)
+    X_train_transformed = preprocessor.transform(X_train)
+
+    explanation = create_shap_explanation(
+        X_train_transformed, X_test_transformed, classifier
+    )
     shap.plots.beeswarm(explanation, show=True, max_display=25)
 
 
@@ -52,6 +62,18 @@ def split_fitted_pipeline(pipeline: ImblearnPipeline) -> tuple[SklearnPipeline, 
     preprocessor = SklearnPipeline(steps=transformer_steps)
     classifier = pipeline[-1]
     return preprocessor, classifier
+
+
+def create_shap_explanation(
+    X_train_transformed: pd.DataFrame,
+    X_test_transformed: pd.DataFrame,
+    classifier: BaseEstimator,
+) -> Explanation | list[Explanation]:
+    masker = shap.maskers.Independent(
+        X_train_transformed, max_samples=len(X_train_transformed)
+    )
+    explainer = shap.Explainer(classifier, masker=masker)
+    return explainer(X_test_transformed)
 
 
 if __name__ == "__main__":
