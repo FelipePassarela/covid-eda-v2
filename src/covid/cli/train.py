65095f30e6afdd5a53a29a7d007e3ff15fe82614
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-07-31 06:09:15 -0300

    refactor(training): ♻️ clean up training script

--- src/covid/cli/train.py
+++ src/covid/cli/train.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from typing import Any
 
 import joblib
+import pandas as pd
 import typer
 from loguru import logger
 from sklearn import clone
@@ -15,10 +17,10 @@ def main() -> None:
     constants.LOGS_DIR.mkdir(parents=True, exist_ok=True)
     logger.add(constants.LOGS_DIR / "train.log", rotation="5 MB")
 
-    typer.run(train_model)
+    typer.run(train)
 
 
-def train_model(
+def train(
     train_data: Path = constants.INTERIM_TRAIN_DATA_PATH,
     output_path: Path = constants.MODELS_DIR / "final_model.joblib",
     scoring: str = "balanced_accuracy",
@@ -26,6 +28,15 @@ def train_model(
     train_data = load_data(train_data)
     X_train, y_train = split_features_and_target(train_data)
 
+    model = train_model(X_train, y_train, scoring)
+
+    report_scores(model, scoring)
+    save_model(model, output_path)
+
+
+def train_model(
+    X_train: pd.DataFrame, y_train: pd.Series[Any], scoring: str
+) -> TunedThresholdClassifierCV:
     cv = RepeatedStratifiedKFold(
         n_splits=5, n_repeats=5, random_state=constants.RANDOM_STATE
     )
@@ -37,14 +48,18 @@ def train_model(
         store_cv_results=True,
     )
     model.fit(X_train, y_train)
+    return model
 
+
+def report_scores(model: TunedThresholdClassifierCV, scoring: str) -> None:
     scoring_formatted = scoring.replace("_", " ")
     logger.info("Best cross-validated {}: {:.4f}", scoring_formatted, model.best_score_)
     logger.info("Selected decision threshold: {:.4f}", model.best_threshold_)
 
+
+def save_model(model: Any, output_path: Path) -> None:
     output_path.parent.mkdir(parents=True, exist_ok=True)
     joblib.dump(model, output_path)
-
     logger.success("Trained model saved to {}", output_path)
 
 
