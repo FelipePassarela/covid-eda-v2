91a8a2fbf10464ea7df3e31857ef43f0d39c2bba
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-09-18 00:10:52 -0300

    refactor(mrmr): 🧵 set n_jobs to 1 in MRMRSelector

--- src/covid/common/feature/mrmr_selector.py
+++ src/covid/common/feature/mrmr_selector.py
@@ -7,12 +7,15 @@ from sklearn.utils.validation import check_is_fitted
 
 
 class MRMRSelector(BaseEstimator, TransformerMixin):
-    def __init__(self, k: int = 250) -> None:
+    def __init__(self, k: int = 50, n_jobs: int = 1) -> None:
+        self.n_jobs = n_jobs
         self.selected_features_ = None
         self.k = k
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> Self:
-        self.selected_features_ = mrmr_classif(X=X, y=y, K=self.k)
+        self.selected_features_ = mrmr_classif(
+            X=X, y=y, K=self.k, n_jobs=self.n_jobs, show_progress=True
+        )
         return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
